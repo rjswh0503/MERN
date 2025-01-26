bffc901b541932b1aa187d6be0a1868a80c60b5f
rjswh0503 <rjswh0503@yahoo.com>
2025-01-26 20:20:22 +0900

    Navgation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
 
 const App = () => {
   return (
     <Router>
+      <MainNavigation />
+      <main>
       <Routes>
           <Route path="/" element={<Users />}/>
           <Route path="/places/new" element={<NewPlace />}/>
       </Routes>
+      </main>
     </Router>
   );
 };
---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+import './MainNavigation.css';
+import MainHeader from "./MainHeader";
+
+const MainNavigation = props => {
+    return <MainHeader>
+        <button className="main-navgation__menu-btn">
+            <span />
+            <span />
+            <span />
+        </button>
+        <h1 className="main-navigation__title">
+        <Link to="/">YourPlaces</Link> 
+        </h1>
+        <nav>
+            ...
+        </nav>
+    </MainHeader>
+};
+
+export default MainNavigation;
+
