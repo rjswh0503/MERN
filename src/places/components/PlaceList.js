290914ed6462dd47ba72a01059412ef007ad592d
rjswh0503 <rjswh0503@yahoo.com>
2025-02-03 20:09:22 +0900

    PlaceItem

---
+++ src/places/components/PlaceList.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Card from '../../shared/components/UIElements/Card';
+import './PlaceList.css';
+import PlaceItem from "./PlaceItem";
+
+const PlaceList = props => {
+
+    if(props.items.length === 0){
+        return <div className="place-list center">
+            <Card>
+                <h2>장소를 찾을 수 없습니다. </h2>
+                <button>장소 공유</button>
+            </Card>
+
+        </div>
+    }
+
+
+    return <ul className="place-list">
+        {props.items.map(place => <PlaceItem 
+        key={place.id} 
+        id={place.id} 
+        image={place.imageUrl} 
+        title ={place.title} 
+        description = {place.description} 
+        address ={place.address} 
+        creatorId={place.creator} 
+        coorinates={place.location}
+         />
+          )}
+    </ul>
+}
+
+export default PlaceList;
