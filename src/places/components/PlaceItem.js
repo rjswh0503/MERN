3c13d3c42e0205f48b6e001ef7cceb0c68c47911
rjswh0503 <rjswh0503@yahoo.com>
2025-02-06 20:49:23 +0900

    modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,31 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 import './PlaceItem.css';
 
 
 const PlaceItem = props => {
 
-    return <li className="place-item">
-        <Card className="place-item__content">
-            <div className="place-item__image">
-                <img src={props.image} alt={props.title} />
-            </div>
-            <div className="place-item__info">
-                <h2>{props.title}</h2>
-                <h3>{props.address}</h3>
-                <p>{props.description}</p>
+    const [showMap, setShowMap] = useState(false);
 
-            </div>
-            <div className="place-item__actions">
-                <Button inverse>지도 보기</Button>
-                <Button to={`/places/${props.id}`}>수정</Button>
-                <Button danger >삭제</Button>
-            </div>
-        </Card>
-    </li>;
+
+    const openMapHandler = () => {
+        setShowMap(true);
+
+    }  
+
+    const closeMapHandler = () => {
+        setShowMap(false);
+    }
+
+    return ( 
+        <React.Fragment>
+            <Modal show={showMap}
+             onCancel={closeMapHandler}
+              header={props.address}
+               contentClass="place-item__modal-content"
+               footerClass="place-item__modal-actions"
+               footer={<Button onClick={closeMapHandler}>닫기</Button>}
+            >
+                <div className="map-container">
+                    <h2>맵임!</h2>
+                </div>
+            </Modal>
+            <li className="place-item">
+                <Card className="place-item__content">
+                    <div className="place-item__image">
+                        <img src={props.image} alt={props.title} />
+                    </div>
+                    <div className="place-item__info">
+                        <h2>{props.title}</h2>
+                        <h3>{props.address}</h3>
+                        <p>{props.description}</p>
+
+                    </div>
+                    <div className="place-item__actions">
+                        <Button inverse onClick={openMapHandler}>지도 보기</Button>
+                        <Button to={`/places/${props.id}`}>수정</Button>
+                        <Button danger >삭제</Button>
+                    </div>
+                </Card>
+            </li>
+        </React.Fragment>
+    );
 }
 
 export default PlaceItem;
--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
 
 import Backdrop from './BackDrop'
 import './Modal.css';
 
 
-const ModalOberlay = props => {
+const ModalOverlay = props => {
     const content = (
         <div className={`modal ${props.className}`} style={props.style}>
             <header className={`modal__header ${props.headerClass}`}>
                 <h2>{props.header}</h2>
             </header>
-            <form onSubmit={props.onSubmit ? props.onSubmit : (event) => event.preventDefault}>
+            <form 
+            onSubmit={
+                props.onSubmit ? props.onSubmit : event => event.preventDefault()}>
                 <div className={`modal__content ${props.contentClass}`}>
                     {props.children}
                 </div>
@@ -22,19 +24,25 @@ const ModalOberlay = props => {
             </form>
         </div>
     );
-    return ReactDOM.createPortal(CountQueuingStrategy, document.getElementById('modal-hook'))
+    return ReactDOM.createPortal(content, document.getElementById('modal-hook'))
 
 
 };
 
 const Modal = props => {
-    return <React.Fragment>
-        {props.show && <Backdrop onClick={props.onCancel}/>}
-        <CSSTransition in={props.show} mountOnEnter unmountOnExit timeout={200} classNames="modal">
-            <ModalOberlay {...props}/>
-        </CSSTransition>
-    </React.Fragment>
-
+    return (
+        <React.Fragment>
+            {props.show && <Backdrop onClick={props.onCancel}/>}
+            <CSSTransition 
+            in={props.show} 
+            mountOnEnter 
+            nmountOnExit 
+            timeout={200} 
+            classNames="modal">
+                <ModalOverlay {...props}/>
+            </CSSTransition>
+        </React.Fragment>
+        );
 };
 
 export default Modal;
