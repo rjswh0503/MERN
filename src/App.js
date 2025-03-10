f1df3309ce72999ee07b67b8f09a787f2a975e0d
rjswh0503 <rjswh0503@yahoo.com>
2025-03-10 20:44:09 +0900

    JsonWebToken

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
@@ -16,16 +16,34 @@ const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+
+
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
-    localStorage.setItem('userData', JSON.stringify({userId: uid, token}));
+    // 토큰 발급 후 토큰 유효기간을 보여주는 코드  1000 * 60 * 60은 1시간
+    // 현재 시간 new Date()에  new Date().getTime()을 해서 1시간을 더한 Date 객체를 얻음
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem('userData', JSON.stringify({
+      userId: uid,
+      token,
+      expiration: tokenExpirationDate.toISOString()
+    }));
     setUserId(uid);
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
+
+    }
+  }, [login]);
 
   let routes;
 
