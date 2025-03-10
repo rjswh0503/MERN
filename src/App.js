2c8e108260a655be9546b077a769ad9d8bcb3610
rjswh0503 <rjswh0503@yahoo.com>
2025-03-10 21:07:36 +0900

    login&logout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 
 import Auth from './user/pages/Auth';
 import Users from './user/pages/Users';
@@ -9,42 +8,15 @@ import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
 import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
+
+
+
 
 const App = () => {
 
-
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
-
-
-
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    // 토큰 발급 후 토큰 유효기간을 보여주는 코드  1000 * 60 * 60은 1시간
-    // 현재 시간 new Date()에  new Date().getTime()을 해서 1시간을 더한 Date 객체를 얻음
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    localStorage.setItem('userData', JSON.stringify({
-      userId: uid,
-      token,
-      expiration: tokenExpirationDate.toISOString()
-    }));
-    setUserId(uid);
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-    localStorage.removeItem('userData');
-  }, []);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
-      login(storedData.userId, storedData.token, new Date(storedData.expiration));
-
-    }
-  }, [login]);
-
+  const { token, login, logout, userId } = useAuth();
+  
   let routes;
 
   if (token) {
